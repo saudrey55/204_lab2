851a4ccfa8fe3b16e1a5a9c22a2bb61fc94762ad
saudrey <weerawutearth@gmail.com>
2021-02-11 17:29:59 +0700

    emoji

--- 6220502183_2_4.c
+++ 6220502183_2_4.c
@@ -1,60 +1,27 @@
 #include<stdio.h>
+#include<string.h>
 
 int main()
 {
-    char name;
-    int num,z;
+    int BD,think;
+    char alpha;
+    char emo[2][3][6] = {{"(^_^)","(*o*)","(T_T)"},{"{@_@}","{*v*}","{x_x}"}};
+    char choose[6];
+    scanf("%c",&alpha);
+    scanf("%d",&BD);
 
-    scanf("%c",&name);
-    scanf("%d",&num);
-    z=name-num;
-    if (z%2 !=0)
-    {
-        if (z%5 != 0)
-        {
-           if (name>='A'&&name<='I')
-            {
-                printf("(^_^)");
-            }
-            else if (name>='J'&&name<='R')
-            {
-                printf("(*o*)");
-            }
-            else if (name>='S'&&name<='Z')
-            {
-                printf("(T_T)");
-            }
-        }
-        
-        else if (z%5 ==0)
-        {
-            if (name>='A'&&name<='I')
-            {
-                printf("%c(^_^)%c",92,47);
-            }
-            else if (name>='J'&&name<='R')
-            {
-                printf("%c(*o*)%c",92,47);
-            }
-            else if (name>='S'&&name<='Z')
-            {
-                printf("%c(T_T)%c",92,47);
-            }
-        }
-    }
-    else if (z%2==0)
-    {
-        if (name>='A'&&name<='I')
-            {
-                printf("{@_@}");
-            }
-            else if (name>='J'&&name<='R')
-            {
-                printf("{*v*}");
-            }
-            else if (name>='S'&&name<='Z')
-            {
-                printf("{x_x}");
-            }
-    }    
+    think = alpha+1-BD;
+    if(alpha>='A' && alpha<='I')
+        strcpy(choose,emo[think%2][0]);
+    else if(alpha>='J' && alpha<='R')
+        strcpy(choose,emo[think%2][1]);
+    else
+        strcpy(choose,emo[think%2][2]);
+
+    if(BD%10==5)
+        printf("%c%s/\n",92,choose);
+    else
+        puts(choose);
+
+    return 0;
 }

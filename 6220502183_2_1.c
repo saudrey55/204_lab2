23db5cc99f29c811c95e72b8890f23ef13085f54
saudrey <weerawutearth@gmail.com>
2021-02-11 14:50:01 +0700

    Grading

--- 6220502183_2_1.c
+++ 6220502183_2_1.c
@@ -2,24 +2,45 @@
 
 int main()
 {
-    int a,b,c,sum;
+    int a,b,c,grade;
 
     scanf ("%d",&a);
     scanf ("%d",&b);
     scanf ("%d",&c);
 
-    sum = a+b+c;
+    grade= a+b+c;
 
-    switch (sum)
+    if (grade >= 80) 
     {
-    case sum >=80:printf("A");break;
-    case sum >=75:printf("B+");break;
-    case sum >=70:printf("B");break;
-    case sum >=65:printf("A");break;
-    case sum >=60:printf("A");break;
-    case sum >=55:printf("A");break;
-    case sum >=50:printf("A");break;
-    default:
-        break;
+    printf("A");
+    } 
+    else  if (grade >= 75) 
+    {
+    printf("B+");
+    } 
+    else  if (grade >= 70) 
+    {
+    printf("B");
+    } 
+    else  if (grade >= 65) 
+    {
+    printf("C+");
+    } 
+    else  if (grade >= 60) 
+    {
+    printf("C");
+    } 
+    else  if (grade >= 55) 
+    {
+    printf("D+");
+    } 
+    else  if (grade >= 50) 
+    {
+    printf("D");
+    } 
+    else
+    {
+        printf("F");
     }
+    
 }

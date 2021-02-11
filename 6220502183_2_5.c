3743ff3b65e3901cd0832bafa38b0f6c327ba0c6
saudrey <weerawutearth@gmail.com>
2021-02-11 17:36:44 +0700

    first

---
+++ 6220502183_2_5.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main()
+{
+    int a,b,z,day;
+    scanf("%d %d",&a,&b);
+
+    switch (b)
+    {
+    case 2:z=28;break;
+    case 4 :
+    case 6 :
+    case 9 :
+    case 11: z=30;break;
+    default: z=31;break;
+    }
+
+    day = ((a+z)%7)+1;
+    printf("%d",&day);    
+}

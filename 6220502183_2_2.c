9b77d74cc7d58045cee8224fc4e8441c114df993
saudrey <weerawutearth@gmail.com>
2021-02-11 15:53:46 +0700

    promotion

--- 6220502183_2_2.c
+++ 6220502183_2_2.c
@@ -11,17 +11,31 @@ int main()
 
     if (pro == 'A')
     {
+        if(a>200)
+        {
         a=a-200;
         b=(a-(int)a)*100/20;
         a=(int)a*3;
         sum=199+a+b;
+        }
+        else
+        {
+            sum=199;
+        }
     }
     else if (pro == 'B')
     {
-       a=a-400;
-       b=(a-(int)a)*100/30;
+        if(a>400)
+        {
+        a=a-400;
+        b=(a-(int)a)*100/30;
         a=(int)a*2;
         sum=299+a+b;
+        }
+        else
+        {
+            sum=299;
+        }
     }
     printf("%.2f",sum);
     
---
+++ 6220502183_2_3.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main()
+{
+    int a1,a2,a3;
+    char z[5];
+
+    
+}

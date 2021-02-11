5b70c37a4a0557cfb4ad2c0f42185c534d5c2126
saudrey <weerawutearth@gmail.com>
2021-02-11 14:57:36 +0700

    grade

--- 6220502183_2_1.c
+++ 6220502183_2_1.c
@@ -7,40 +7,46 @@ int main()
     scanf ("%d",&a);
     scanf ("%d",&b);
     scanf ("%d",&c);
-
+    if (a>=0&&a<=30&&b>=0&&b<=30&&c>=0&&c<=40)
+    {
     grade= a+b+c;
 
     if (grade >= 80) 
     {
-    printf("A");
+        printf("A");
     } 
     else  if (grade >= 75) 
     {
-    printf("B+");
+        printf("B+");
     } 
     else  if (grade >= 70) 
     {
-    printf("B");
+        printf("B");
     } 
     else  if (grade >= 65) 
     {
-    printf("C+");
+        printf("C+");
     } 
     else  if (grade >= 60) 
     {
-    printf("C");
+        printf("C");
     } 
     else  if (grade >= 55) 
     {
-    printf("D+");
+        printf("D+");
     } 
     else  if (grade >= 50) 
     {
-    printf("D");
+        printf("D");
     } 
     else
     {
         printf("F");
     }
+
+    return 0;
+    }
+    
+  
     
 }

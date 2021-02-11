9dc5b5f73f574734b5a4a8c64ac25ccb48d7bfe3
saudrey <weerawutearth@gmail.com>
2021-02-11 14:43:12 +0700

    Update 6220502183_2_1.c

--- 6220502183_2_1.c
+++ 6220502183_2_1.c
@@ -2,10 +2,24 @@
 
 int main()
 {
-    int a,b,c;
+    int a,b,c,sum;
 
     scanf ("%d",&a);
     scanf ("%d",&b);
     scanf ("%d",&c);
-    
+
+    sum = a+b+c;
+
+    switch (sum)
+    {
+    case sum >=80:printf("A");break;
+    case sum >=75:printf("B+");break;
+    case sum >=70:printf("B");break;
+    case sum >=65:printf("A");break;
+    case sum >=60:printf("A");break;
+    case sum >=55:printf("A");break;
+    case sum >=50:printf("A");break;
+    default:
+        break;
+    }
 }

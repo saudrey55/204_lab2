b4ba30a5ac6d845a38980b8c0326708db362b241
saudrey <weerawutearth@gmail.com>
2021-02-11 16:17:17 +0700

    ABC

--- 6220502183_2_3.c
+++ 6220502183_2_3.c
@@ -2,8 +2,36 @@
 
 int main()
 {
-    int a1,a2,a3;
-    char z[5];
+    int z=3,a[z],i,j;
+    char s[z];
 
+   for ( i = 0; i < z; i++)
+   {
+       scanf("%d",&a[i]);
+   }
+
+   for ( i = 0; i < z; i++)
+   {
+       scanf(" %c",&s[i]);
+   }
+   
+   i = 0;
+    do
+    {
+        if(i < z-1 && a[i] > a[i+1])
+        {
+            j = a[i+1];
+           a[i+1] = a[i];
+            a[i] = j;
+            i = 0;
+        }
+        else
+        {
+            i++;
+        }
+         
+    }while(i < z);
+   
+   
     
 }
